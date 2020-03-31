29da76004ff9f2401df58fb9240cb30fb4715533
shashankd48 <jfriday464@gmail.com>
2020-04-01 00:56:05 +0530

    .env created, Enviroment variables changed

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
 
 // First connecting to the database
-mongoose.connect('mongodb://localhost:27017/tshirt', {
+mongoose.connect(process.env.DATABASE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true
 }).then(()=>{
     console.log('DB CONNECTED');
-})
+});
 
 // creating server with express
-const port = 8000;
+const port = process.env.Port || 8000;
 app.listen(port, ()=>{
     console.log(`App is running at ${port}`);
 })
