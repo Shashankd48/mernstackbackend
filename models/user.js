940127101fd03d70449f3f7eb7610e41022cd30c
shashankd48 <jfriday464@gmail.com>
2020-03-31 20:31:05 +0530

    First commit

---
+++ models/user.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+
+// if its not working try old above method on importing
+import { v1 as uuidv1 } from 'uuid';
+
+var userSchema = new mongoose.Schema({
+  name: {
+      type: String,
+      required: true,
+      maxlength: 32,
+      trim: true
+  },
+  lastname:{
+    type: String,
+    maxlength: 32,
+    trim: true
+  },
+  email: {
+      type: String,
+      trim: true,
+      required: true,
+      unique: true
+  },
+  userinfo: {
+      type: String,
+      trim: true
+  },
+    encry_password: {
+        type: String,
+        required: true
+    },
+    salt: String,
+    rol: {
+        type: Number,
+        default: 0
+    },
+    purchases: {
+        type: Array,
+        default: []
+    }
+},{ timestamps: true});
+
+userSchema.virtual('password')
+    .set((password)=>{
+        this._password = password;
+        this.salt = uuidv1();
+        this.encry_password = this.securePassword(password);
+    })
+    .get(()=>{
+        return this._password;
+    })
+
+
+userSchema.method = {
+
+    authenticate: function(plainPassowrd){
+        return this.securePassword(plainPassowrd) === this.encry_password;
+    },
+    securePassword: function(plainPassowrd){
+        if(!password) return "";
+        try {
+            return crypto.createHmac('sha256', this.salt)
+                .update(plainPassowrd)
+                .digest('hex');
+        } catch (error) {
+            return "";   
+        }
+    }
+}
+
+module.exports = mongoose.model('User',userSchema)
