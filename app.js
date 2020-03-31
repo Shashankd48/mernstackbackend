2faf1b7c26bbf12fb3704206a01636d362771aa7
shashankd48 <jfriday464@gmail.com>
2020-04-01 01:36:57 +0530

    3-middleware added in app.js

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
 
 // First connecting to the database
 mongoose.connect(process.env.DATABASE, {
@@ -11,6 +15,11 @@ mongoose.connect(process.env.DATABASE, {
 }).then(()=>{
     console.log('DB CONNECTED');
 });
+
+// Our middleware
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(cors());
 
 // creating server with express
 const port = process.env.Port || 8000;
