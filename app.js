9b7eeac0b6808c07ab4c544ae1e8301ed06b6f23
shashankd48 <jfriday464@gmail.com>
2020-04-01 11:33:41 +0530

    authRoutes is created. API added

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
+const authRoutes = require('./routes/auth');
 
 // First connecting to the database
 mongoose.connect(process.env.DATABASE, {
@@ -16,13 +17,18 @@ mongoose.connect(process.env.DATABASE, {
     console.log('DB CONNECTED');
 });
 
-// Our middleware
+// My middleware
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors());
 
+// My Routes
+app.use('/api', authRoutes);
+
 // creating server with express
 const port = process.env.Port || 8000;
+
+// Starting a server
 app.listen(port, ()=>{
     console.log(`App is running at ${port}`);
 })
